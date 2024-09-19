4331a9611b5f62a6285739c397106fc5acf62997
Anamulhaque7 <anamulhaque7537@gmail.com>
2024-09-19 09:39:29 +0600

    add money section

---
+++ js/login.js
@@ -0,0 +1,22 @@
+document.getElementById('button-login').addEventListener('click', function (event) {
+    event.preventDefault();
+
+
+    const phoneNumber = document.getElementById('phone-number').value;
+    const pinNumber = document.getElementById('pin-number').value;
+
+    if (phoneNumber === '5' && pinNumber === '1234') {
+        window.location.href = '/home.html'
+    }
+    else {
+        alert('ples chack you number or pin')
+    }
+
+
+
+
+
+
+
+
+})
