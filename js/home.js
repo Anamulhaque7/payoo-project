19813d7bbe0e615bbbfb89c037e16ed42786a4b9
Anamulhaque7 <anamulhaque7537@gmail.com>
2024-09-19 10:10:10 +0600

    added partailly the add money

--- js/home.js
+++ js/home.js
@@ -1,1 +1,18 @@
-console.log('home dsafjdifjdiwo')
+// Add money to the acount 
+
+
+// Step 1: add money an event handler to the add button inside the form
+document.getElementById('btn-add-money').addEventListener('click',function(event){
+    // Prevent page reload after form submit
+event.preventDefault();
+
+// step  2: get money to be added to the account
+
+const addMoney = document.getElementById('input-add-money').value;
+console.log(addMoney)
+
+// grt the pin number
+const pinNumber = document.getElementById('input-pin-number').value;
+console.log(pinNumber)
+
+})
